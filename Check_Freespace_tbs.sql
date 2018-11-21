a770e18641f34b6f826ab5da4a852afee3da908d
Live Wire65 <live_wire65@yahoo.com>
2018-11-21 13:46:12 +0200

    Check Freespace_tbs

---
+++ Check_Freespace_tbs.sql
@@ -0,0 +1,10 @@
+-- IJK check free space for tablespace data files
+-- 2013-05-28
+
+ accept tbsname format A50 prompt "Enter tablespace name: ";
+
+ select tablespace_name,file_id,sum(blocks) as "Free space in blocks", sum(bytes)/1024/1024 as "Free space in Megabytes"
+ from dba_free_space where tablespace_name = '&tbsname' group by tablespace_name, file_id;
+ 
+ select file_name,file_id,bytes, maxbytes,status, autoextensible, online_status 
+from dba_data_files where tablespace_name = '&tbsname';
